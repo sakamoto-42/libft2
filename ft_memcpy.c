1825f61b3af8618216a1d81f7abf06d2cd4d4bd4
julien <julien66140@hotmail.fr>
2024-11-04 22:04:55 +0100

    wip tests

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: julien <julien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/04 18:29:38 by juduchar          #+#    #+#             */
-/*   Updated: 2024/11/04 21:48:06 by julien           ###   ########.fr       */
+/*   Updated: 2024/11/04 22:04:44 by julien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,11 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned char		*dest_ptr;
 	const unsigned char	*src_ptr;
+
+	/*
+	if (!dest || !src)
+		return (NULL);
+	*/
 
 	dest_ptr = (unsigned char *) dest;
 	src_ptr = (const unsigned char *) src;
